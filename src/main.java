9606cb34b8ef3eb32c6e30be45abf0aff5f1ff6c
shamhpopaty <sham.moon2002@gmail.com>
2023-12-07 14:55:49 +0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class main {
+    public void main(String args[]){
+        System.out.println("i am programmer");
+        System.out.println("i am sham");
+        System.out.println("i am batoul");
+
+    }
+}
---
+++ src/package-info.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class info {
+    public void main(String args[]){
+        JFrame frame=new JFrame();
+        JButton button=new JButton();
+        button.setBounds(140,200,100,100);
+        frame.add(button);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
+}
